43dd948f47049c435211eeb9bfc9b2f1ff35238f
Hajar-Srd <hajarserrad@gmail.com>
2021-06-20 20:00:47 +0100

    Create Dataset

---
+++ .history/model_20210623014403.py
@@ -0,0 +1,28 @@
+WORKSPACE_PATH = 'workspace'
+SCRIPTS_PATH = 'scripts'
+APIMODEL_PATH = 'models'
+ANNOTATION_PATH = WORKSPACE_PATH + '/annotations'
+IMAGE_PATH = WORKSPACE_PATH + '/images'
+MODEL_PATH = WORKSPACE_PATH +'/models'
+PRETRAINED_MODEL_PATH = WORKSPACE_PATH + '/pre-trained-models'
+CONFIG_PATH = MODEL_PATH + '/my_ssd/mobnet/pipeline.config'
+CHECKPOINT_PATH = MODEL_PATH + '/my_ssd_mobnet/'
+
+
+labels = [
+    {'name':'a', 'id':1}, 
+    {'name':'f', 'id':6}
+]
+
+with open(ANNOTATION_PATH + '\label_map.pbtxt', 'w') as f:
+    for label in labels:
+        f.write('item { \n')
+        f.write('\tname:\'{}\'\n'.format(label['name']))
+        f.write('\tid:{}\n'.format(label['id']))
+        f.write('}\n')
+
+
+!python {SCRIPTS_PATH + '/generate_tfrecord.py'} -x {IMAGE_PATH + '/train'} -l {ANNOTATION_PATH + '/label_map.pbtxt'} --output_path = ANNOTATION_PATH + '/train.record'
+!python {SCRIPTS_PATH + '/generate_tfrecord.py'} -x {IMAGE_PATH + '/test'} -l {ANNOTATION_PATH + '/label_map.pbtxt'} -o {ANNOTATION_PATH + '/test.record'}
+
+
